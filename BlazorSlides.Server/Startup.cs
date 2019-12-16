6f8bda9ed3ff1318ebb0650c45cc2edf6646a7b4
Stavros Kasidis <s.kasidis@epsilonnet.gr>
2019-12-16 10:58:15 +0200

    upgrade to 3.1

--- BlazorSlides.Server/Startup.cs
+++ BlazorSlides.Server/Startup.cs
@@ -33,14 +33,16 @@ namespace BlazorSlides.Server
                 app.UseBlazorDebugging();
             }
 
+            app.UseStaticFiles();
+            app.UseClientSideBlazorFiles<Client.Startup>();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapDefaultControllerRoute();
+                endpoints.MapFallbackToClientSideBlazor<Client.Startup>("index.html");
             });
-
-            app.UseBlazor<Client.Startup>();
         }
     }
 }
